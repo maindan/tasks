8863ea23c968663859761208f3270778447d736b
Daniel <danielima.projex@gmail.com>
2025-04-24 13:00:16 -0400

    feat: register task model in admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from app.models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'first_value', 'second_value', 'result', 'created_at',)
+    search_fields = ('name',)
+    list_filter = ('name',)
