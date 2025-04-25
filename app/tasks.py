f1bc7061c5e7310893871d76e65139d4e4982a46
Daniel <danielima.projex@gmail.com>
2025-04-25 09:32:41 -0400

    feat: overriding create method to start task

--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,14 @@
 from celery import shared_task
+from rest_framework.generics import get_object_or_404
+from .models import Task
 
 @shared_task
-def sum(x, y):
-    return x + y
+def sum(id: int):
+    try:
+        task = get_object_or_404(Task, id=id)
+        result = task.first_value + task.second_value
+        task.result = result
+        task.save()
+        return {"resultado": f"task finalizada, resultado: {task.result}"}
+    except Exception as ex:
+        return str(ex)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
-
+from .tasks import sum
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        sum.delay(serializer.data["id"])
+        return Response({"message": "Task criada com sucesso!", "data": serializer.data})
